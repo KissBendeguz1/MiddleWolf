ec529260880ec02fae486e345ad927b7f1d32ec2
KissBendeguz1 <113110759+KissBendeguz1@users.noreply.github.com>
2026-01-07 19:33:10 +0100

    Add files via upload

---
+++ js/LoadSideNavBar.js
@@ -0,0 +1,53 @@
+function Load() {
+    body = document.body;
+    body.innerHTML = `<nav>
+      <section class="nav-container">
+        <img id="nav-logo-big" class="nav-img" src="../assets/Logo1.png" alt="" />
+      </section>
+      <section class="nav-container">
+        <input
+          type="text"
+          name="navsearch"
+          id="search"
+          placeholder="Search..."
+        />
+        <div id="nav-profile">
+          <img
+            id="nav-profile-img"
+            class="nav-img"
+            src="../assets/nav-logo-placeholder.jpg"
+            alt=""
+          />
+          <h1>Név</h1>
+        </div>
+      </section>
+    </nav>
+    <section class="sidebar-container">
+      <div class="sidebar-item-container">
+        <div class="sidebar-item">
+          <img class="sidebar-icon" src="../assets/main.png" alt="" />
+          <h2>Főoldal</h2>
+        </div>
+        <div class="sidebar-item">
+          <img
+            class="sidebar-icon"
+            src="../assets/about.png"
+            alt=""
+          />
+          <h2>Rólunk</h2>
+        </div>
+
+        <div class="sidebar-item">
+          <img class="sidebar-icon" src="../assets/map.png" alt="" />
+          <h2>Térkép</h2>
+        </div>
+        <div class="sidebar-item">
+          <img class="sidebar-icon" src="../assets/contact.png" alt="" />
+          <h2>Kapcsolat</h2>
+        </div>
+      </div>
+      <div class="sidebar-bottom">
+        <img id="sidebar-setting" src="../assets/settings.png" alt="">
+      </div>
+    </section>`;
+}
