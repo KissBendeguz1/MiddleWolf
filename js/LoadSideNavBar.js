27ef76f70c9884e0d319fac3837f1d38a3529194
kissbendi1 <kissb@kkszki.hu>
2026-03-04 20:37:01 +0100

    Responsivity fixed on all sites

--- js/LoadSideNavBar.js
+++ js/LoadSideNavBar.js
@@ -39,6 +39,7 @@ function checkUserType() {
       } else {
         console.error("Hiba a profil lekérése során: " + xhr.status);
       }
+      ManageNav()
     }
   };
 
@@ -182,6 +183,8 @@ document.addEventListener(
     e.preventDefault();
     checkUserType();
   },
+
+
   /*if (sessionStorage.getItem("Type") === "Ceges") { */
   /* const sideNavBarHTML = `<hr id="logged_in_navbar_hr">
     <a href="../html/compamy-register.html">
@@ -244,3 +247,56 @@ document.addEventListener(
                 
             </a>`*/
 );
+
+function ManageNav() {
+  const sidebar = document.getElementsByClassName("sidebar-container")[0];
+  const nav = document.getElementsByClassName("nav-container")[0];
+  console.log(nav);
+  const menudiv = document.createElement("div");
+  menudiv.classList.add("hidden");
+  menudiv.id = "sidemenuopen";
+  menudiv.innerHTML += `
+<svg class="svg-invert" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`;
+  menudiv.addEventListener("click", () => { 
+    sidebar.classList.toggle("hidden");
+  });
+  menudiv.setAttribute("onload", "LoadNavMenu()");
+  nav.prepend(menudiv);
+    function toggleNav() {
+      
+    }
+
+  function LoadNavMenu() {
+    screenwidth = window.innerWidth;
+    if (screenwidth <= 1400) {
+      console.log(screenwidth);
+      NavMobile();
+    } else {
+      NavDesktop();
+    }
+
+    window.addEventListener("resize", () => {
+      screenwidth = window.innerWidth;
+      if (screenwidth <= 1400) {
+        console.log(screenwidth);
+        NavMobile();
+      } else {
+        NavDesktop();
+      }
+    });
+  }
+
+  function NavMobile() {
+    menudiv.classList.remove("hidden");
+    sidebar.classList.add("hidden");
+  }
+
+  function NavDesktop() {
+    menudiv.classList.add("hidden");
+    sidebar.classList.remove("hidden");
+  }
+
+
+
+  LoadNavMenu()
+};
