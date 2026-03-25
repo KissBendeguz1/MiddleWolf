21d2153215e5021a3c44903ba6fdf1bf18f4237a
KissBendeguz1 <113110759+KissBendeguz1@users.noreply.github.com>
2026-03-25 23:40:09 +0100

    showalert fix

--- js/ShowAlert.js
+++ js/ShowAlert.js
@@ -1,4 +1,4 @@
-export function ShowAlert(message, type) {
+function ShowAlert(message, type) {
   // Store alert in sessionStorage for persistence after reload
   sessionStorage.setItem(
     "pendingAlert",
