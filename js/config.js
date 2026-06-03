eece1fbc6d803a973f911d37612f1b3fc1092972
KissBendeguz1 <113110759+KissBendeguz1@users.noreply.github.com>
2026-06-03 15:06:08 +0200

    changed ip

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  */
 
 // API Server IP Address
-const API_IP = "78.92.110.240";
+const API_IP = "78.92.121.25";
 const API_PORT = "6969";
 const API_BASE_URL = `http://${API_IP}:${API_PORT}/api`;
 console.log(`API Base URL: ${API_BASE_URL}`);
